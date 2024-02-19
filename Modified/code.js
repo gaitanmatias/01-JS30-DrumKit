05f610b9a159c3fbe4412370c3780af1d256c945
Matias Gaitan <gaitanmatiasg@gmail.com>
2024-02-19 02:01:39 -0300

    new project folder added

--- Modified/code.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("keydown", function(e){
-    let codeKey = e.keyCode;
-    let audioKey = document.querySelector(`audio[data-key="${codeKey}"]`);
-    let key = document.querySelector(`section[data-key="${codeKey}"]`);
-    audioKey.currentTime = 0;
-    audioKey.play();
-    key.classList.add("playing");
-    audioKey.addEventListener("ended", function(){key.classList.remove("playing")});
-});
